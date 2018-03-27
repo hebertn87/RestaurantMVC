6af3b822f5fa7718692c74ae96e27a2f1ed982ad
Nathan <hebertn87@gmail.com>
2018-03-26 23:57:01 -0600

    Pushed final not completely working version

--- src/tableModel/TableModel.java
+++ src/tableModel/TableModel.java
@@ -4,6 +4,8 @@ package tableModel;
 import java.util.Observable;
 import java.util.Observer;
 
+import observerForms.TableStateNotify;
+
 public class TableModel extends Observable implements Observer {
 	
 	TableState ready;
@@ -11,14 +13,15 @@ public class TableModel extends Observable implements Observer {
 	TableState needsCleaning;
 	TableState beingCleaned;
 	TableState state;
+	int tablePosition;
 	
 	//Set's the initial table states
-	public TableModel() {
+	public TableModel(int _tablePosition) {
 		ready = new Ready(this);
 		inUse = new InUse(this);
 		needsCleaning = new NeedsCleaning(this);
 		beingCleaned = new BeingCleaned(this);
-		
+		tablePosition = _tablePosition;
 		state = ready;
 	}
 	
@@ -79,12 +82,17 @@ public class TableModel extends Observable implements Observer {
 						this.getState().toString();
 				//If from user
 				else {
-					if(state.toString().equals("ReadyTable"))
+					if(state.toString().equals("ReadyTable")) {
 						state.IsReady();
-					else if(state.toString().equals("InUseTable"))
+						this.notifyObservers(new TableStateNotify(tablePosition, getState().toString()));
+					
+					}
+					else if(state.toString().equals("InUseTable")) {
 						state.IsInUse();
-				}
+						this.notifyObservers(new TableStateNotify(tablePosition, getState().toString()));
 		
+					}
+				}
 	}
 	
 }
